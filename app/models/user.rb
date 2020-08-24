8134dcc1426877a57075b787b4dc7fba15f9fa8a
tomoya4130 <m.tomoya0314@gmail.com>
2020-08-24 18:32:51 +0900

    Userバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_save { self.email = email.downcase }
+
+  with_options presence: true do
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])\w{6,}\z/
+    validates :nickname
+    validates :email, format: { with: VALID_EMAIL_REGEX , message: "は＠マークを含む必要があります。"}, uniqueness: { case_sensitive: false } #一意性制約
+    validates :password, format: { with: VALID_PASSWORD_REGEX, message: "は半角6文字以上英数字それぞれ１文字以上含む必要があります"}
+    validates :password, confirmation: true
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
+    validates :ruby_last_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+    validates :ruby_first_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+    validates :birthday
+  end
 end
