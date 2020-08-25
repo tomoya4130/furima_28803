dfa76d671f4da85528255c999c5b9bc5cb1d3cf7
tomoya4130 <m.tomoya0314@gmail.com>
2020-08-25 20:43:25 +0900

    トップページの表示とログイン、ログアウトの設定

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,7 @@
+class ItemsController < ApplicationController
+  #before_action :authenticate_user!, expect: :index
+
+  def index
+    @items = Item.all
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_091702) do
+ActiveRecord::Schema.define(version: 2020_08_25_075646) do
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
+    t.string "name", null: false
+    t.text "text", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -30,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_08_21_091702) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
