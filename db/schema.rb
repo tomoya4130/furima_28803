e72c3326573ab18b0e89d613f6cf7bb95d926d94
tomoya4130 <m.tomoya0314@gmail.com>
2020-08-26 19:28:53 +0900

    itemテーブルのカラム編集

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,11 @@ ActiveRecord::Schema.define(version: 2020_08_26_033715) do
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "text", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "shipping_fee_burden_id", null: false
+    t.integer "shipping_region_id", null: false
+    t.integer "days_until_shipping_id", null: false
     t.integer "price", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
