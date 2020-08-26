3cb591784eb1447164dce7edb36a27fef72f305e
tomoya4130 <m.tomoya0314@gmail.com>
2020-08-26 12:13:08 +0900

    ログインしているユーザーのみ出品ページに遷移できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,21 @@
 class ItemsController < ApplicationController
-  # before_action :authenticate_user!, expect: :index
+  #before_action :authenticate_user!, expect: :index
+  before_action :login_check, only: :new
 
   def index
     @items = Item.all
+  end
+
+  def new
+    @items = Item.new
+  end
+
+  private
+
+  def login_check
+    unless user_signed_in?
+      flash[:alert] = "ログインしてください"
+      redirect_to new_user_session_path
+    end
   end
 end
