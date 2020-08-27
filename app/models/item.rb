9f79a7d79dcd1ba7a6d54790da813a3aa6fbe3ab
tomoya4130 <m.tomoya0314@gmail.com>
2020-08-27 12:02:58 +0900

    ActiveHashの設定とitemモデルとのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one :purchase
   has_one_attached :image
+  
+  #ActiveHashのアソシエーション
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_fee_burden
+  belongs_to_active_hash :shipping_region
+  belongs_to_active_hash :days_until_shipping
 
+  #空の投稿を保存できないようにする
   with_options presence: true do
     validates :image
     validates :name, length: { maximum: 40 }
     validates :text, length: { maximum: 1000 }
     validates :price, format: {with: /\A[0-9]+\z/, message: "Half-width number"}
   end
+
+  #ActiveHashの選択が「ーーー」のときは保存できないようにする
   validates :category_id, numericality: { other_than: 0, message: "Select"}
   validates :status_id, numericality: { other_than: 0, message: "Select"}
   validates :shipping_fee_burden_id, numericality: { other_than: 0, message: "Select"}
