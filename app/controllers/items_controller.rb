ad8cc6dd76e3a8ec0a117048ee326964966838a8
tomoya4130 <m.tomoya0314@gmail.com>
2020-08-27 16:06:07 +0900

    商品を出品したらトップページに表示される

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,14 +3,24 @@ class ItemsController < ApplicationController
   before_action :login_check, only: :new
 
   def index
-    @items = Item.all
+    @items = Item.includes(:user).order("created_at DESC") #新規投稿順に並び替える
   end
 
   def new
-    @items = Item.new
+    @item = Item.new #itemテーブルに新しいレコードを追加する
+  end
+
+  def create
+    @item = Item.new(item_params) #データの保存に成功したらトップページへ、失敗したら出品ページへ遷移する
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
   end
 
   private
+  
   #ログインしていない場合登録画面に遷移する
   def login_check
     unless user_signed_in?
@@ -18,6 +28,12 @@ class ItemsController < ApplicationController
       redirect_to new_user_session_path 
     end
   end
+  
+  #createでこのデータが保存されるかどうか判断する
+  def item_params
+    params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :shipping_fee_burden_id, :shipping_region_id, :days_until_shipping_id, :price).merge(user_id: current_user.id)
+  end
+
   #ActiveStorageによりログインしているユーザーが画像を添付した際にitemテーブルにあるnameとpriceカラムの情報も受け取るようにしている（itemとActiveStorageはアソシエーションにより紐づいている）
   def message_params
     params.require(:item).permit(:image, :name, :price).merge(user_id: current_user.id)
