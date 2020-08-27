546abc1d22d816af73c6c05ece5beeee38ba2987
tomoya4130 <m.tomoya0314@gmail.com>
2020-08-27 22:55:04 +0900

    itemのFactoryBot

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,16 @@
 FactoryBot.define do
   factory :item do
+    after(:build) do |item|
+      item.images.attach(io: File.open('spec/fixtures/test_image.jpg'), filename: 'test_image.jpg', content_type: 'image/jpg')
+    end
+    name { Faker::Name.initials(number: 2) }
+    text { Faker::Lorem.sentence }
+    price { Faker::Number.number(digits: 5) }
+    category_id {"1"}
+    status_id {"1"}
+    shipping_fee_burden_id {"1"}
+    shipping_region_id {"1"}
+    days_until_shipping_id {"1"}
+    user
   end
 end
