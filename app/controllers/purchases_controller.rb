7677ba52767fe93b63060ece785cd2a6545ce538
tomoya4130 <m.tomoya0314@gmail.com>
2020-08-31 22:12:05 +0900

    クレジット　controller　設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,9 +9,29 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    
+    @purchase = Purchase.new(item_id: purchase_params[:item_id], user_id: purchase_params[:user_id])
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      return redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
 
+  def purchase_params
+    params.permit(:item_id, :user_id, :token)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_868ce3e4cb726eddddc57616"
+    Payjp::Charge.create(
+      item_id: purchase_params[:item_id], #商品のid
+      user_id: purchase_params[:user_id], #出品者のid
+      card: purchase_params[:token],   #カードトークン
+      currency: 'jpy'                  #通過の種類（日本円）
+    )
+  end
 end
