2c42f617a4a8069ceb377df1bb70687de846b441
tomoya4130 <m.tomoya0314@gmail.com>
2020-08-31 15:53:56 +0900

    出品者が購入画面に遷移できない設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,34 +1,18 @@
 class PurchasesController < ApplicationController
   before_action :login_check, only: :index
-
+  before_action :authenticate_user!
   def index
-    @purchase = Purchase.new
     @item = Item.find(params[:item_id])
+    return redirect_to root_path if current_user.id == @item.user_id #出品者は購入画面にいけないようにしている
+    @purchase = Purchase.new
+   
     # @purchases = @item.purchase.includes(:user)
   end
 
   def create
-    #@item = Item.find(params[:item_id])
-    # @purchase = Purchase.new(purchase_params) # データの保存に成功したらトップページへ、失敗したら出品ページへ遷移する
-    # if @purchase.save
-    # redirect_to items_path
-    # else
-    # render :index
-    # end
+    
   end
 
   private
-
-  # ログインしていない場合登録画面に遷移する
-  def login_check
-    unless user_signed_in?
-      flash[:alert] = 'ログインしてください'
-      redirect_to new_user_session_path
-    end
-  end
-
-  def purchase_params
-    params.require(:purchase).permit(user_id: current_user.id, item_id: params[:item_id])
-  end
 
 end
